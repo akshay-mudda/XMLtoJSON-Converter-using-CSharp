5f49f8474f52e526a2597d61d6a14fb5e2fa1a31
akshaymu <akshaymu@ICS-LT-66Q1ZD3>
2024-09-27 21:32:13 +0530

    Add project files.

---
+++ XMLtoJSON-Converter-using-CSharp/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.IO;
+using System.Configuration;
+using Newtonsoft.Json;
+using System.Xml;
+
+namespace XMLtoJSON_Converter_using_CSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Read source and destination paths from app.config
+            string sourcePath = ConfigurationManager.AppSettings["SourcePath"];
+            string destinationPath = ConfigurationManager.AppSettings["DestinationPath"];
+
+            // Check if destination folder exists, create if not
+            if (!Directory.Exists(destinationPath))
+            {
+                Directory.CreateDirectory(destinationPath);
+            }
+
+            // Get all XML files in the source folder
+            string[] xmlFiles = Directory.GetFiles(sourcePath, "*.xml");
+
+            if (xmlFiles.Length == 0)
+            {
+                Console.WriteLine("No XML files found in the source folder.");
+                return;
+            }
+
+            foreach (var xmlFilePath in xmlFiles)
+            {
+                try
+                {
+                    // Get file name without extension
+                    string fileNameWithoutExtension = Path.GetFileNameWithoutExtension(xmlFilePath);
+                    string jsonFilePath = Path.Combine(destinationPath, fileNameWithoutExtension + ".json");
+
+                    // Check if the JSON already exists
+                    if (File.Exists(jsonFilePath))
+                    {
+                        Console.WriteLine($"JSON already exists for {fileNameWithoutExtension}. Skipping file.");
+                        continue;
+                    }
+
+                    // Convert XML file to JSON
+                    ConvertXmlToJson(xmlFilePath, jsonFilePath);
+
+                    // After conversion, delete the source XML file
+                    File.Delete(xmlFilePath);
+
+                    Console.WriteLine($"Successfully converted {fileNameWithoutExtension}.xml to JSON.");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Error converting {xmlFilePath}: {ex.Message}");
+                }
+            }
+        }
+
+        // Convert XML file to JSON using Newtonsoft.Json
+        static void ConvertXmlToJson(string xmlFilePath, string jsonFilePath)
+        {
+            // Load the XML document
+            XmlDocument xmlDoc = new XmlDocument();
+            xmlDoc.Load(xmlFilePath);
+
+            // Convert XML to JSON with formatting (specifying the namespace)
+            string jsonText = JsonConvert.SerializeXmlNode(xmlDoc, Newtonsoft.Json.Formatting.Indented);
+
+            // Write JSON to the destination file
+            File.WriteAllText(jsonFilePath, jsonText);
+        }
+    }
+}
---
+++ XMLtoJSON-Converter-using-CSharp/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("XMLtoJSON-Converter-using-CSharp")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("XMLtoJSON-Converter-using-CSharp")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("c70c7771-c4b8-4b94-af2c-a3f5f0a65349")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
